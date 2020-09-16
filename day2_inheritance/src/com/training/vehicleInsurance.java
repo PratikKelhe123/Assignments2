f7511f42f6cbef7bbd34f31fa05850aff93a48e1
Pratik Kelhe <pratik.kelhe16@vit.edu>
2020-09-16 16:44:18 +0530

    later part

---
+++ day2_inheritance/src/com/training/vehicleInsurance.java
@@ -0,0 +1,63 @@
+package com.training;
+
+public class vehicleInsurance extends Insurance {
+
+	private String vehicleModel;
+	private int costOfManufacture;
+	private int yearOfManufacture;
+	
+	public vehicleInsurance(int policyNumber, String policyHolderName, int policyHolderAge, double insuredAmount) {
+		super(policyNumber, policyHolderName, policyHolderAge, insuredAmount);
+		// TODO Auto-generated constructor stub
+	}
+	public vehicleInsurance(int policyNumber, String policyHolderName, int policyHolderAge, double insuredAmount,
+			String vehicleModel, int costOfManufacture, int yearOfManufacture) {
+		super(policyNumber, policyHolderName, policyHolderAge, insuredAmount);
+		this.vehicleModel = vehicleModel;
+		this.costOfManufacture = costOfManufacture;
+		this.yearOfManufacture = yearOfManufacture;
+	}
+	public String getVehicleModel() {
+		return vehicleModel;
+	}
+	public void setVehicleModel(String vehicleModel) {
+		this.vehicleModel = vehicleModel;
+	}
+	public int getCostOfManufacture() {
+		return costOfManufacture;
+	}
+	public void setCostOfManufacture(int costOfManufacture) {
+		this.costOfManufacture = costOfManufacture;
+	}
+	public int getYearOfManufacture() {
+		return yearOfManufacture;
+	}
+	public void setYearOfManufacture(int yearOfManufacture) {
+		this.yearOfManufacture = yearOfManufacture;
+	}
+	@Override
+	public double calulatePremium() {
+		// TODO Auto-generated method stub
+		
+		if( this.vehicleModel.equals("Two") ) {
+			if(this.yearOfManufacture-2020 < 2) {
+			return 0.5*this.getInsuredAmount();
+			}
+		else {
+			return 0.6*this.getInsuredAmount();
+		}}else {
+			if(this.yearOfManufacture-2020 < 2) {
+				return 0.4*this.getInsuredAmount();
+				}
+			else {
+				return 0.*this.getInsuredAmount();
+			}
+		}
+		
+	
+	
+	}
+	
+	
+
+}
